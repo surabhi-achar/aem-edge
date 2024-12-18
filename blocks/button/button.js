094c7e257013c8780e99dd392f13a106f24e77e6
Surabhi Achar <surabhiachar@Surabhis-MacBook-Pro.local>
2024-12-18 13:29:20 +0530

    button custom

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,19 +1,15 @@
-
 export default function decorate(block) {
     const alignment = block.dataset.alignment || 'align-left';
-    const alignmentClasses = ['align-left', 'align-center', 'align-right'];
+    console.log('Alignment applied:', alignment);
   
-    // Remove existing alignment classes
+    const alignmentClasses = ['align-left', 'align-center', 'align-right'];
     block.classList.remove(...alignmentClasses);
     block.classList.add(alignment);
   
-    // Update button container alignment
     const buttonContainer = block.querySelector('.button-container');
     if (buttonContainer) {
       buttonContainer.classList.remove(...alignmentClasses);
       buttonContainer.classList.add(alignment);
-    } else {
-      console.warn('Button container not found!');
     }
   }
   
